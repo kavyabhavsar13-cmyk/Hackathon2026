7a28b7e9d13b24cbcbd45b15e328540cb127a085
HarshvardhanDoriya26 <hkdoriya07@gmail.com>
2026-07-12 10:48:23 +0530

    Trip Model has been added.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+
 const connectDB = require("./config/db");
 
 const vehicleRoutes = require("./routes/vehicleRoutes");
+const tripRoutes = require("./routes/tripRoutes");
 
 dotenv.config();
 
@@ -17,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/vehicles", vehicleRoutes);
+app.use("/api/trips", tripRoutes);
 
 app.get("/", (req, res) => {
   res.send("TransitOps API Running");
